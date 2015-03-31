8f747cae88e2b66c124dd8a08a673e9dbf14a3b4
Brian Dooley <brian.dooley@feedhenry.com>
2015-03-31 16:48:31 +0100

    colors

--- www/hello.js
+++ www/hello.js
@@ -1,20 +1,22 @@
 document.getElementById('go').onclick = function () {
-  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
+  document.getElementById('connectResponse').innerHTML = "<p style=background-color:white>Connecting.....</p>";
   $fh.cloud(
       {
-        path: 'hello',
+        path: 'connect',
+        method: "GET",
         data: {
           host: document.getElementById('host').value,
           port: document.getElementById('port').value,
+          database: document.getElementById('database').value,
           user: document.getElementById('user').value,
           password: document.getElementById('password').value,
         }
       },
       function (res) {
-        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+        document.getElementById('connectResponse').innerHTML = "<p style=background-color:green>" + res.msg + "</p>";
       },
       function (code, errorprops, params) {
-        alert('An error occured: ' + code + ' : ' + errorprops);
+        document.getElementById('connectResponse').innerHTML = "<p style=background-color:red>" + errorprops.message + "</p>";
       }
   );
 };
